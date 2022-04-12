c166db04987d0671268bdef7a432752929dbe219
fuchenxu2008 <fuchenxu2008@163.com>
2022-04-11 22:23:36 -0700

    path added

--- week_2/Seven.js
+++ week_2/Seven.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
+const path = require('path')
 
-const words = fs.readFileSync(`../${process.argv[2]}`, 'utf8').toLowerCase().match(/[a-z]{2,}/ig);
-const stopWords = new Set(fs.readFileSync('../stop_words.txt', 'utf8').split(','));
+const words = fs.readFileSync(path.resolve(__dirname, `../${process.argv[2]}`), 'utf8').toLowerCase().match(/[a-z]{2,}/ig);
+const stopWords = new Set(fs.readFileSync(path.resolve(__dirname, '../stop_words.txt'), 'utf8').split(','));
 const freqMap = words.filter(w => !stopWords.has(w)).reduce((acc, w) => { acc[w] = (acc[w] || 0) + 1; return acc; }, {});
 Object.entries(freqMap).sort(([, a], [, b]) => b - a).slice(0, 25).forEach(([w, c]) => console.log(w, '-', c))
